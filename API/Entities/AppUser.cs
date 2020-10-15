3cdf84dc853901521fe534b273d56dcc85ca76cd
Rob Mays <rob@earthit.co.uk>
2020-10-15 10:45:33 +0100

    added users controller and DbContext

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        //properties need to be publie to work with Entity framework
+        // use Id as for entity framework later it will create an autoincrement
+        public int Id { get; set; }
+        // case on UserName is crucial for when we implemnet asp Identity done use case = Username it will create lot less refactoring
+        public string UserName { get; set; }
+
+    }
+}
