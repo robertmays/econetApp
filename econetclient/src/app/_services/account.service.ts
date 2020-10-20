ad872a91adf8acd396f7355b4f976e4bac713093
Rob Mays <rob@earthit.co.uk>
2020-10-20 10:48:16 +0100

    added logedin dropdown menu

--- econetclient/src/app/_services/account.service.ts
+++ econetclient/src/app/_services/account.service.ts
@@ -1,18 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  
+@Injectable({  
   providedIn: 'root'
 })
 export class AccountService {
   //singleton while application lifetime, so data here live while client in the browser
   //whereas components are destroyed as soon as they go out of use
-  baseUrl = "https//localhost:5001/api/";
+  baseUrl = 'https://localhost:5001/api/';
 
   constructor(private http: HttpClient) { }
 
   login(model: any){
-    return this.http.post(this.baseUrl + 'accounts/login', model);
+    return this.http.post(this.baseUrl + 'account/login', model);
   }
 }
