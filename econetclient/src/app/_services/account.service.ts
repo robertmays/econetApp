e0a1b3d67bfef77d5a5e3aa7e3f7bd5da2ef9c56
Rob Mays <rob@earthit.co.uk>
2020-10-20 09:06:36 +0100

    added our first service

---
+++ econetclient/src/app/_services/account.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  
+  providedIn: 'root'
+})
+export class AccountService {
+  //singleton while application lifetime, so data here live while client in the browser
+  //whereas components are destroyed as soon as they go out of use
+  baseUrl = "https//localhost:5001/api/";
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any){
+    return this.http.post(this.baseUrl + 'accounts/login', model);
+  }
+}
