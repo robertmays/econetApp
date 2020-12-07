652ceefe7376dce9a97e67eb15dd7a75f5269e83
Rob Mays <rob@earthit.co.uk>
2020-12-07 11:07:11 +0000

    added user photo gallery and tabs

--- econetclient/src/app/employees/employee-detail/employee-detail.component.ts
+++ econetclient/src/app/employees/employee-detail/employee-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { Employee } from 'src/app/_models/employee';
 import { EmployeesService } from 'src/app/_services/employees.service';
 
@@ -10,20 +11,49 @@ import { EmployeesService } from 'src/app/_services/employees.service';
 })
 export class EmployeeDetailComponent implements OnInit {
   employee: Employee;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private employeeService: EmployeesService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // this is a synchronus operation thing will happen one after the other (but no waiting for things to complete)
     //remember Rob your html page will load before this completes
     //so any interpolation on the html page will be undefind
     //so add *ngIf="employee" to your html tags to make sure our employee is available
     this.loadEmployee();
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];    
+  }
+
+  getImages(): NgxGalleryImage[] {
+    const imageUrls = [];
+    for (const photo of this.employee.photos) {
+      imageUrls.push({
+        small: photo?.url,
+        medium: photo?.url,
+        big: photo?.url
+      });
+    }
+
+    return imageUrls;
   }
 
   loadEmployee() {
     this.employeeService.getEmployee(this.route.snapshot.paramMap.get('id')).subscribe(employee => {
       console.log(employee);
       this.employee = employee;
+      //now i got the employee i can set the property below
+      this.galleryImages = this.getImages();
     });
   }
 
